d3f0b1fd6371d8c6d6888bcaece91b5636b0df1c
arulmozhikumar7 <arulmozhikumar7@gmail.com>
2025-03-11 16:01:40 +0530

    speed insights

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ import Notifications from "/src/components/feedbacks/Notifications.jsx"
 import ConfirmationWindow from "/src/components/modals/ConfirmationWindow.jsx"
 import {useFeedbacks} from "/src/providers/FeedbacksProvider.jsx"
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
     const {listImagesForCache} = useData()
 
